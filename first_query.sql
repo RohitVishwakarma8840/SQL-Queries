15eb7afdccabaebc135c2fb9fb9773f954277358
Rohit <rohit8840185@gmail.com>
2024-12-12 19:56:12 +0530

    Adding q

--- first_query.sql
+++ first_query.sql
@@ -5,3 +5,14 @@ create database temp2;
 create database college;
 drop database temp1;
 drop database temp2;
+
+create database college;
+
+USE college;
+
+CREATE TABLE student(
+id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT NOT NULL
+
+);
