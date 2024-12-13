3ae960a662a405f7a2d5bee76b03f4d1b9191fce
Rohit <rohit8840185@gmail.com>
2024-12-13 19:17:22 +0530

    pk

---
+++ Second/Primary_key_Concept.sql
@@ -0,0 +1,12 @@
+CREATE TABLE temp1(
+id INT,
+name VARCHAR(100),
+age INT,
+city VARCHAR(100),
+PRIMARY KEY(id,name)  -- primary key kaa combination kabhi bhi duplicate nahi ho saktaa
+-- that's the important point in selecting the terminal 
+
+
+);
+
+SELECT * FROM temp1;
