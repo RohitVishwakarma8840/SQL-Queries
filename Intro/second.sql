7ada6d18992b09e1b6409d15780d8051df9a9029
Rohit <rohit8840185@gmail.com>
2024-12-13 14:32:17 +0530

    Adding code

---
+++ Intro/second.sql
@@ -0,0 +1,20 @@
+create database college;
+create database if not exists college;
+
+USE college;
+
+CREATE TABLE student(
+id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT NOT NULL
+
+);
+
+INSERT INTO student VALUES(1,"Rohit",21);
+INSERT INTO student VALUES(2,"Logan Paul",30);
+
+SELECT * FROM student;
+
+use college;
+show databases;
+SHOW TABLES;
